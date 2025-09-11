4c7e67d07d254df68dfe28238e0ba4663ba136aa
sabbirkhanparmar-oss <sabbirkhanparmar@gmail.com>
2025-09-11 10:56:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav a').forEach(link => {
- link.addEventListener('click', e => {
-   e.preventDefault();
-   const target = document.querySelector(link.getAttribute('href'));
-   if(target) target.scrollIntoView({behavior:'smooth'});
- });
-});
